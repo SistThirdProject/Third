ae004e1cc45ed8981edaa18c0b28ed573fd7fb02
wnsdu <wnsdu@DESKTOP-3IVJMG0>
2018-06-29 00:19:01 +0900

    필요없는 클래스 삭제

--- src/main/java/com/sist/blog/NaverBlog.java
+++ src/main/java/com/sist/blog/NaverBlog.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -23,52 +24,42 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class NaverBlog {
 
 	
-	/*private static WebDriver driver;
-    String Title = null;
-    String URL = null;
-    String alertText = "";
- 
-    @BeforeClass
-    public static void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "/home/sist/data/chromedriver.exe"); //크롬 드라이버 파일 경로설정
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); //응답시간 5초설정
-        driver.get("http://blog.naver.com/ocublog?logNo=221308619983");  //접속할 사이트
-    }
-   //blog 본문 class명 - se_component se_paragraph default 
-    
-    @Test
-    public static void blogCrwl () throws Exception {
-         
-    	WebElement element = driver.findElement(By.className("se_component se_paragraph default"));    
-        //blog 본문 내용 찾기
-    	String blogContext=element.getText();
-        System.out.println(blogContext);
- 
-    }
-    
- 
-    @AfterClass
-    public static void tearDown() throws Exception {
-    	
-        driver.quit();
-    }*/
+	
  
     
     public static void main(String[] args) 
     { 
-    	//크롬 드라이버 파일 경로설정
-        System.setProperty("webdriver.chrome.driver", "/home/sist/selenium/chromedriver"); 
+    	//�겕濡� �뱶�씪�씠踰� �뙆�씪 寃쎈줈�꽕�젙
+        //System.setProperty("webdriver.chrome.driver", "/home/sist/selenium/chromedriver"); 
+        System.setProperty("webdriver.chrome.driver", "c:\\download\\chromedriver.exe");
         
-        // 크롬 드라이버 객체 생성
+        // �겕濡� �뱶�씪�씠踰� 媛앹껜 �깮�꽦
      	 WebDriver driver = new ChromeDriver();
      	
-     	//blog 접속
+     	/*WebDriverWait wait = new WebDriverWait(driver, 10);
+     	
+     	WebElement element = wait.Until(ExpectedConditions.elementToBeClickable(By.id("someid")));*/		
+     	//blog �젒�냽
  		driver.get("http://blog.naver.com/ocublog?logNo=221308619983");
+ 		long start=System.currentTimeMillis();
+ 		System.out.println(start);
  		
- 		//접속한 사이트의 클래스명이 일치하는 것 가져오기
- 		List<WebElement> element = driver.findElements(By.className("se_editView se_textView se_textarea"));
+ 		try {
+			Thread.sleep(20000);
+			
+			long end=System.currentTimeMillis();
+			System.out.println(end);
+			System.out.println("실행시간:"+(end - start)/1000.0+"초");
+			
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
  		
+ 		
+ 		//�젒�냽�븳 �궗�씠�듃�쓽 �겢�옒�뒪紐낆씠 �씪移섑븯�뒗 寃� 媛��졇�삤湲�
+ 		//List<WebElement> element = driver.findElements(By.className("se_textarea"));
+ 		//List<WebElement> element = driver.findElements(By.tagName("h3"));
+ 		List<WebElement> element = driver.findElements(By.cssSelector("#mainFrame span.dairy.aged div.se_textView"));
  		for(WebElement s:element)
  		{
  			System.out.println(s.getText());
